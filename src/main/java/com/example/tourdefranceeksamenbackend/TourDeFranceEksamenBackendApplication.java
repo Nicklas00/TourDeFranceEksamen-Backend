7d95a801573c530c3122d3231e60034af39e48e0
Nicklas00 <nick553h@stud.kea.dk>
2022-05-30 12:38:18 +0200

    Initial commit

---
+++ src/main/java/com/example/tourdefranceeksamenbackend/TourDeFranceEksamenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourdefranceeksamenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourDeFranceEksamenBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TourDeFranceEksamenBackendApplication.class, args);
+  }
+
+}
